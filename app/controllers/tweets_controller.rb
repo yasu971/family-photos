7b945b04966d3518f02d2c5cf9cef651ae9a003b
yasu971 <yasuishii1421@gmail.com>
2020-12-22 12:05:40 +0900

    Update tweets_controller.rb
    リダイレクト処理を用意

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @tweets = Tweet.all
@@ -30,12 +31,17 @@ class TweetsController < ApplicationController
   end
 
   private
-
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
   end
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
-  end  
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
